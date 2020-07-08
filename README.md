b197f970b11c6ad4cc622b5e9588778f9ed2a454
katiehawcutt <katiehawcutt@gmail.com>
2020-07-08 09:37:13 +0100

    added README

---
+++ README.md
@@ -0,0 +1,12 @@
+# ShoeBot App
+
+###### May 2020
+
+_I made this app in order to control a little robot my partner built. It can go forwards, backwards, left and right!_
+
+### Main learning points:
+
+- I was able to practice using fetch() again but instead of using a get request like I had for the Kayne West Quote Generator, I used a post request (because I wanted to send data to ShoeBot).
+- The user selects the directions by clicking on the relevant image button. This data is then stored in a JavaScript object which is later stringified before being sent to ShoeBot.
+- I learnt about JSON.stringify: When sending data to a web server, the data has to be a string. JSON.stringify() converts a JavaScript object into a JSON string. If you wanted to convert data back again after receiving it you would use JSON.parse().
+- I used the DOM appendChild to make the users chosen directions show underneath the controls by inserting the arrow image that they clicked on into a div container. This was good practice as I'd not done this much before and I was really pleased with how it worked.
--- main.js
+++ main.js
@@ -8,11 +8,6 @@ function addDirections(direction) {
   console.log(dataToSend);
 }
 
-// create an image tag
-// set the source of the image tag to the relevant path (to the image)
-//select the div where we are going to put the image
-//make image a child of the div using apphendChild
-
 function userChoice(direction) {
   var image = document.createElement("img");
   if (direction == "forward") {
@@ -27,21 +22,6 @@ function userChoice(direction) {
   document.querySelector(".directions-container").appendChild(image);
   console.log("user log called");
 }
-
-// function userChoice(direction) {
-//   if (direction == "forward") {
-//     document.querySelector(".directions-container");
-
-//     getElementById("userChoice").src += "./bluearrow-forward.png";
-//   } else if (direction == "left") {
-//     document.getElementById("userChoice").src += "./bluearrow-left.png";
-//   } else if (direction == "right") {
-//     document.getElementById("userChoice").src += "./bluearrow-right.png";
-//   } else {
-//     document.getElementById("userChoice").src += "./bluearrow-backward.png";
-//   }
-//   console.log(userChoice);
-// }
 
 function runShoebot() {
   fetch("http://192.168.1.111:5000/sequence", {
